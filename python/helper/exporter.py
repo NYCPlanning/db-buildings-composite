a491666a55019056099757d54b802edc987a5204
mgraber <molly.graber@me.com>
2020-02-12 19:30:48 +0000

    Vectorize pad string cleaning and sorting

---
+++ python/helper/exporter.py
@@ -0,0 +1,60 @@
+from .engines import edm_engine, qaqc_engine, psycopg2_connect
+from sqlalchemy.types import TEXT
+import io
+import psycopg2
+import logging
+import os
+
+def exporter(df, output_table, DDL, 
+            con=qaqc_engine, sql='', sep=',', 
+            geo_column='', SRID=4326):
+    
+    # Parse output table
+    schema = output_table.split('.')[0]
+    version = output_table.split('.')[1].replace('"', '')
+
+    # psycopg2 connections
+    db_connection = psycopg2_connect(con.url)
+    db_cursor = db_connection.cursor()
+    str_buffer = io.StringIO() 
+
+    columns = [i.strip('"') for i in DDL.keys()]
+    column_definitions = ','.join([f'"{key}" {value}' for key,value in DDL.items()])
+
+    # Create table
+    create = f'''
+    CREATE SCHEMA IF NOT EXISTS {schema};
+    DROP TABLE IF EXISTS {output_table};
+    CREATE TABLE {output_table} (
+        {column_definitions}
+    );
+    '''
+
+    con.connect().execute(create)
+    con.dispose()
+
+    os.system(f'\necho "exporting table {output_table} ..."')
+
+    # If the intermediate output is spatial, add SRID to spatial columns
+    df = df[columns]
+
+    if geo_column != '':
+        df.loc[:, geo_column] = df.loc[:, geo_column].apply(lambda x: f'SRID={SRID};{x}' if x else '')
+    else: 
+        pass
+
+    # export
+    df.to_csv(str_buffer, sep=sep, header=False, index=False)
+    str_buffer.seek(0)
+    db_cursor.copy_from(str_buffer, output_table, sep=sep, null='')
+    db_cursor.connection.commit()
+
+    # additional queries or transformations:
+    if sql != '':
+        con.connect().execute(sql)
+        con.dispose()
+    else: pass
+
+    str_buffer.close()
+    db_cursor.close()
+    db_connection.close()
--- python/pad.py
+++ python/pad.py
@@ -1,140 +1,50 @@
-from sqlalchemy import create_engine
+from helper.engines import recipe_engine, edm_engine, build_engine
 import pandas as pd
+import re
 import os
-import csv
 
-def build_address_string(row, street_numbers, no_street_number):
-    # lhnd and hhnd come in with extra spaces, hence the stripping
-    lhnd = row['lhnd'].strip()
-    hhnd = row['hhnd'].strip()
-    stname = row['stname'].strip()
+def hnum_sort_and_list(street_df):
+    # Sort number addresses, join, and append street name
+    sorted_hnum = street_df.sort_values('hnum')
+    hnum_list = sorted_hnum['hnum'].unique().tolist()
+    address_string = ','.join(hnum_list)
+    
+    return address_string
 
-    if len(lhnd) == 0 and len(hhnd) == 0:
-        #no_street_number.append(stname)
-        no_street_number.append(" ".join(stname.split()))
-    else:
-        if lhnd == hhnd:
-            street_numbers.append(lhnd)
-        else:
-            if len(hhnd) == 0:
-                street_numbers.append(lhnd)
-            else:
-                street_numbers.append(lhnd + " - " + hhnd)
-
-    return street_numbers, no_street_number
+def sort_and_list(bin_df):
+    # Temporarily modify street name field before alphabetizing
+    bin_df.loc[bin_df['hnums'] == '', 'stname'] = 'ZZZ' + bin_df['stname']
+    sorted_bin = bin_df.sort_values('stname')
+    
+    # Remove modifications and join
+    sorted_bin['stname'] = sorted_bin['stname'].str.replace('ZZZ','')
+    address_list = sorted_bin['full_address'].unique().tolist()
+    address_string = '::'.join(address_list)
+    
+    return address_string
 
 if __name__ == '__main__':
-    # Connect to postgres db
-    recipe_engine = create_engine(os.environ['RECIPE_ENGINE'])
-    engine = create_engine(os.environ['BUILD_ENGINE'])
-
-    # Get the PAD records from the database. Filter out "million" BINs.
-
+    # Get the PAD records from recipes, filtering out "million" BINs.
     import_sql = '''SELECT boro, block, lot, bin, lhnd, hhnd, stname
         FROM dcp_pad.latest
         WHERE bin NOT IN ('1000000', '2000000', '3000000', '4000000', '5000000')
         ORDER BY boro, block, lot, bin, stname, lhnd;'''
-
     df = pd.read_sql(import_sql, recipe_engine)
 
-    if df.empty:
-        print('No records on PAD table read')
-        exit()
-    else:
-        print(df.head())
+    # Strip extra spaces from house numbers
+    df['lhnd'] = df['lhnd'].str.strip()
+    df['hhnd'] = df['hhnd'].str.strip()
+    df['stname'] = df['stname'].str.strip()
 
-    # Initialize work areas
+    # Concatenate house num ranges
+    df.loc[df['hhnd'] == df['lhnd'], 'hnum'] = df['lhnd']
+    df.loc[df['hhnd'] != df['lhnd'], 'hnum'] = df['lhnd'] + " - " + df['hhnd']
+    df.loc[df['hhnd'] == '', 'hnum'] = df['lhnd']
 
-    boro_hold = df['boro'].iloc[0]
-    block_hold = df['block'].iloc[0]
-    lot_hold = df['lot'].iloc[0]
-    bin_hold = df['bin'].iloc[0]
-    stname_hold = df['stname'].iloc[0]
-    no_street_number = []
-    street_numbers = []
-    addresses = []
-    c1 = ","
-    c2 = '::'
-    input_ctr = 0
-    output_ctr = 0
-    bin_ctr = 1
+    # Create full address field
+    df_by_street = df.groupby(['bin','stname']).apply(hnum_sort_and_list).reset_index().rename(columns={0:'hnums'})
+    df_by_street.loc[df_by_street['hnums'] != '', 'full_address'] = df_by_street['hnums'] + ' ' + df_by_street['stname']
+    df_by_street.loc[df_by_street['hnums'] == '', 'full_address'] = df_by_street['stname']
 
-    # Open the output CSV, loop through the data and roll up addresses for each BIN
-
-    with open('output/pad_output.csv', mode='w') as pad_output:
-        pad_output_writer = csv.writer(pad_output, delimiter=',', quotechar='"', quoting=csv.QUOTE_ALL)
-        pad_output_writer.writerow(["boro", "block", "lot", "bin", "pad_addr"])
-
-        for index, row in df.iterrows():
-
-            input_ctr += 1
-            if input_ctr % 10000 == 0:
-                print('Records processed {}'.format(input_ctr))
-
-            if row['bin'] != bin_hold:
-
-            # Write the record for the last BIN to the output dataframe
-
-                if len(street_numbers) > 0:
-                    if bin_ctr == 1:
-                        stname_hold = '({})'.format(stname_hold.strip())
-                    #addresses.append(c1.join(sorted(street_numbers)) + " " + stname_hold.strip())
-                    addresses.append(c1.join(sorted(street_numbers)) + " " + " ".join(stname_hold.split()))
-
-                if len(addresses) == 0 and len(no_street_number) == 1:
-                    no_street_number[0] = '({})'.format(no_street_number[0])
-
-                street_string = c2.join([*addresses, *no_street_number])
-
-                pad_output_writer.writerow([boro_hold, block_hold, lot_hold, bin_hold, street_string])
-
-                output_ctr += 1
-                bin_ctr = 0
-
-                # Now start rolling up addresses for the new BIN
-
-                boro_hold = row['boro']
-                block_hold = row['block']
-                lot_hold = row['lot']
-                bin_hold = row['bin']
-                stname_hold = row['stname']
-                no_street_number = []
-                street_numbers = []
-                addresses = []
-                street_string = ''
-                bin_ctr += 1
-
-                street_numbers, no_street_number = build_address_string(row, street_numbers, no_street_number)
-
-            else:
-
-                # BIN is the same. But if the street number has changed, format the old one by combining
-                # the street numbers with the name, and reinitialize the street numbers list.
-
-                if row['stname'] != stname_hold:
-
-                    if len(street_numbers) > 0:
-                        #addresses.append(c1.join(sorted(street_numbers)) + " " + stname_hold.strip())
-                        addresses.append(c1.join(sorted(street_numbers)) + " " + " ".join(stname_hold.split()))
-                        street_numbers = []
-
-                    stname_hold = row['stname']
-                    bin_ctr += 1
-
-                street_numbers, no_street_number = build_address_string(row, street_numbers, no_street_number)
-
-        # Process the last record
-
-        if len(street_numbers) > 0:
-            if bin_ctr == 1:
-                stname_hold = '({})'.format(stname_hold.strip())
-                #addresses.append(c1.join(sorted(street_numbers)) + " " + stname_hold.strip())
-                addresses.append(c1.join(sorted(street_numbers)) + " " + " ".join(stname_hold.split()))
-
-        street_string = c2.join([*addresses, *no_street_number])
-
-        pad_output_writer.writerow([boro_hold, block_hold, lot_hold, bin_hold, street_string])
-        output_ctr += 1
-
-        print('Records read and processed {}'.format(input_ctr))
-        print('Records written {}'.format(output_ctr))
+    df_by_bin = df_by_street.groupby('bin').apply(sort_and_list)
+    print(df_by_bin.head())
