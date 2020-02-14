d1cd5ee158dbf1c0766565bcbee37c4f0cf232b2
mgraber <molly.graber@me.com>
2020-02-14 18:52:06 +0000

    Remove commented code containing looping procedure

--- python/melissa.py
+++ python/melissa.py
@@ -28,17 +28,16 @@ def load_bin_chunk(bin):
 
 def sort_single_bin(df):
     # Create full address field
-    grouped = df.groupby(['boro','block','lot','bin','stname'])
-    print(len(df_by_street))
-    df_by_street = grouped.apply(hnum_sort_and_list)
-    df_by_street = df_by_street.reset_index()
+    df_by_street = df.groupby(['boro','block','lot','bin','stname']).apply(hnum_sort_and_list).reset_index()
     df_by_street = df_by_street.rename(columns={0:'hnums'})
-    print(df_by_street.head())
     df_by_street.loc[df_by_street['hnums'] != '', 'full_address'] = df_by_street['hnums'] + ' ' + df_by_street['stname']
     df_by_street.loc[df_by_street['hnums'] == '', 'full_address'] = df_by_street['stname']
+    
 
     # Sort full addresses
-    df_by_bin = df_by_street.groupby(['boro','block','lot','bin']).apply(sort_and_list)
+    df_by_bin = df_by_street.groupby(['boro','block','lot','bin']).apply(sort_and_list).reset_index()
+    df_by_bin = df_by_bin.rename(columns={0:'usps_addr'})
+    print(df_by_bin.head())
     return df_by_bin
 
 def load_and_sort(bin):
@@ -68,103 +67,3 @@ if __name__ == '__main__':
 
     results_df.to_csv('output/melissa_output.csv')
 
-
-
-
-    '''
-    # Initialize work areas
-
-    boro_hold = df['boro'].iloc[0]
-    block_hold = df['block'].iloc[0]
-    lot_hold = df['lot'].iloc[0]
-    bin_hold = df['bin'].iloc[0]
-    stname_hold = df['stname'].iloc[0]
-    no_street_number = []
-    street_numbers = []
-    addresses = []
-    c1 = ","
-    c2 = '::'
-    input_ctr = 0
-    output_ctr = 0
-    bin_ctr = 1
-
-    # Open the output CSV, loop through the data and roll up addresses for each BIN
-
-    with open('output/melissa_output.csv', mode='w') as melissa_output:
-        melissa_output_writer = csv.writer(melissa_output, delimiter=',', quotechar='"', quoting=csv.QUOTE_ALL)
-        melissa_output_writer.writerow(["boro", "block", "lot", "bin", "usps_addr"])
-
-        for index, row in df.iterrows():
-
-            input_ctr += 1
-            if input_ctr % 10000 == 0:
-                print('Records processed {}'.format(input_ctr))
-
-            if row['bin'] != bin_hold:
-
-            # Write the record for the last BIN to the output dataframe
-
-                if len(street_numbers) > 0:
-                    if bin_ctr == 1:
-                        stname_hold = '({})'.format(stname_hold.strip())
-                    #addresses.append(c1.join(sorted(street_numbers)) + " " + stname_hold.strip())
-                    addresses.append(c1.join(sorted(street_numbers)) + " " + " ".join(stname_hold.split()))
-
-                if len(addresses) == 0 and len(no_street_number) == 1:
-                    no_street_number[0] = '({})'.format(no_street_number[0])
-
-                street_string = c2.join([*addresses, *no_street_number])
-
-                melissa_output_writer.writerow([boro_hold, block_hold, lot_hold, bin_hold, street_string])
-
-                output_ctr += 1
-                bin_ctr = 0
-
-                # Now start rolling up addresses for the new BIN
-
-                boro_hold = row['boro']
-                block_hold = row['block']
-                lot_hold = row['lot']
-                bin_hold = row['bin']
-                stname_hold = row['stname']
-                no_street_number = []
-                street_numbers = []
-                addresses = []
-                street_string = ''
-                bin_ctr += 1
-
-                street_numbers, no_street_number = build_address_string(row, street_numbers, no_street_number)
-
-            else:
-
-                # BIN is the same. But if the street number has changed, format the old one by combining
-                # the street numbers with the name, and reinitialize the street numbers list.
-
-                if row['stname'] != stname_hold:
-
-                    if len(street_numbers) > 0:
-                        #addresses.append(c1.join(sorted(street_numbers)) + " " + stname_hold.strip())
-                        addresses.append(c1.join(sorted(street_numbers)) + " " + " ".join(stname_hold.split()))
-                        street_numbers = []
-
-                    stname_hold = row['stname']
-                    bin_ctr += 1
-
-                street_numbers, no_street_number = build_address_string(row, street_numbers, no_street_number)
-
-        # Process the last record
-
-        if len(street_numbers) > 0:
-            if bin_ctr == 1:
-                stname_hold = '({})'.format(stname_hold.strip())
-                #addresses.append(c1.join(sorted(street_numbers)) + " " + stname_hold.strip())
-                addresses.append(c1.join(sorted(street_numbers)) + " " + " ".join(stname_hold.split()))
-
-        street_string = c2.join([*addresses, *no_street_number])
-
-        melissa_output_writer.writerow([boro_hold, block_hold, lot_hold, bin_hold, street_string])
-        output_ctr += 1
-
-        print('Records read and processed {}'.format(input_ctr))
-        print('Records written {}'.format(output_ctr))
-'''
