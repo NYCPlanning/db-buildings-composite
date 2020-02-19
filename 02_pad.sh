5077ccbe2b18fc6f49358963a7e44ee8f0a5e79a
mgraber <molly.graber@me.com>
2020-02-19 19:13:43 +0000

    Remove old bash scripts that are not inside build folder

--- 02_pad.sh
+++
@@ -1,8 +0,0 @@
-DB_CONTAINER_NAME=bc
-
-docker run -it --rm\
-            --network=host\
-            -v `pwd`:/home/db-buildings-composite\
-            -w /home/db-buildings-composite\
-            --env-file .env\
-            sptkl/docker-geosupport:19d bash -c "pip3 install -r python/requirements.txt; python3 python/pad.py"
