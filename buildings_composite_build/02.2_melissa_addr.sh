6c9529d0ec682ae2928befac80b45d7c385d4ff0
mgraber <molly.graber@me.com>
2020-02-26 17:01:55 +0000

    Remove duplicate python files

--- buildings_composite_build/02.2_melissa_addr.sh
+++ buildings_composite_build/02.2_melissa_addr.sh
@@ -5,4 +5,4 @@ docker run --rm\
     -v `pwd`:/home/buildings_composite_build\
     -w /home/buildings_composite_build\
     --env-file .env\
-    sptkl/cook:latest bash -c "pip3 install -r python/requirements.txt; python3 python/melissa_chunked.py"
+    sptkl/cook:latest bash -c "pip3 install -r python/requirements.txt; python3 python/melissa_looped.py"
